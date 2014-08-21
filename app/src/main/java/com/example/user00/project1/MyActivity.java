53549b0e755f280e5555c5d70efd422f16deedff
pyroclasm <pyro@kset.org>
2014-08-21 17:54:28 +0200

    neka promjena br 1 hhh j

--- app/src/main/java/com/example/user00/project1/MyActivity.java
+++ app/src/main/java/com/example/user00/project1/MyActivity.java
@@ -12,7 +12,7 @@ public class MyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        int i = 2;
+        int i = 2222;
 
     }
 
